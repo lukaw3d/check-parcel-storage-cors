6372d333beb6b3850b664da92b1cc9af6f96d220
lukaw3d <lukajeran@oasislabs.com>
2022-05-06 00:47:51 +0200

    Use parcel-bundler to serve html

--- src/script.js
+++ src/script.js
@@ -3,11 +3,16 @@
 import Parcel from '@oasislabs/parcel';
 console.log(Parcel);
 
-(async () => {
-  const parcel = new Parcel(token);
-  console.log(parcel);
-  console.log(await parcel.getCurrentIdentity());
-  const doc = await parcel.uploadDocument('this is data', null).finished;
-  console.log(doc);
-  await doc.delete();
-})();
+document.querySelector('#form').addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const token = window.token.value;
+  (async () => {
+    const parcel = new Parcel(token);
+    console.log(parcel);
+    console.log(await parcel.getCurrentIdentity());
+    const doc = await parcel.uploadDocument('this is data', null).finished;
+    console.log(doc);
+    await doc.delete();
+  })();
+})
